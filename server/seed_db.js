a6b8b499e0ec619f81edbdd58afbff56eeb6ede8
Thinh Ngo Phan <ngophanthinh@gmail.com>
2022-04-22 21:08:02 -0700

    refactored seed data

--- server/seed_db.js
+++ server/seed_db.js
@@ -1,6 +1,6 @@
 const pool = require('./db.js');
 
-// remember to create an index for product_id column in review table once seeded
+// run 1st 2 seed queries first than 3 than 4
 
 // pool.query(`COPY review FROM '/Users/Thinh2Win/Data/reviews.csv' DELIMITER ',' CSV HEADER;`)
 //   .then(() => {
@@ -8,18 +8,6 @@ const pool = require('./db.js');
 //     pool.query(`CREATE INDEX product_id_index ON review (product_id);`)
 //       .then(() => {
 //         console.log('product_id indexed for review table');
-//       });
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
-
-// pool.query(`COPY reviewPhotos FROM '/Users/Thinh2Win/Data/reviews_photos.csv' DELIMITER ',' CSV HEADER;`)
-//   .then(() => {
-//     console.log('Photos table is seeded');
-//     pool.query(`CREATE INDEX review_id_index ON reviewphotos (review_id);`)
-//       .then(() => {
-//         console.log('review_id indexed for reviewphotos table');
 //       });
 //   })
 //   .catch((err) => {
@@ -38,20 +26,26 @@ const pool = require('./db.js');
 //     console.log(err);
 //   });
 
-// pool.query(`COPY characteristicReviews FROM '/Users/Thinh2Win/Data/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;`)
+// pool.query(`COPY reviewPhotos FROM '/Users/Thinh2Win/Data/reviews_photos.csv' DELIMITER ',' CSV HEADER;`)
 //   .then(() => {
-//     console.log('Characteristic reviews table is seeded');
+//     console.log('Photos table is seeded');
+//     pool.query(`CREATE INDEX review_id_index ON reviewphotos (review_id);`)
+//       .then(() => {
+//         console.log('review_id indexed for reviewphotos table');
+//       });
 //   })
 //   .catch((err) => {
 //     console.log(err);
 //   });
 
-// for setting up indexes that have to be outside
-
-// pool.query(`CREATE INDEX review_id_charecteristic_id_index ON characteristicreviews (review_id, characteristic_id);`)
+// pool.query(`COPY characteristicReviews FROM '/Users/Thinh2Win/Data/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;`)
 //   .then(() => {
-//     console.log('review_id and characteristic_id indexed for review table');
+//     console.log('Characteristic reviews table is seeded');
+//     pool.query(`CREATE INDEX review_id_charecteristic_id_index ON characteristicreviews (review_id, characteristic_id);`)
+//       .then(() => {
+//         console.log('review_id and characteristic_id indexed for review table');
+//       });
 //   })
 //   .catch((err) => {
 //     console.log(err);
-// });
+//   });
